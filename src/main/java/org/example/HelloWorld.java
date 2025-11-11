92fd0cf410ec36192ed90ed03822077483bc47f1
mihaivaidos <mihai@vaidos.com>
2025-11-11 17:29:22 +0200

    Created a simple Spring application using XML configuration.

---
+++ src/main/java/org/example/HelloWorld.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class HelloWorld {
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void printMessage() {
+        System.out.println(message);
+    }
+}
