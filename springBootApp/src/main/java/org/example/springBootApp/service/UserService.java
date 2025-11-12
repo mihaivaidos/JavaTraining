f1f930cce69c0e155d9dd48601f218359c453d68
mihaivaidos <mihai@vaidos.com>
2025-11-12 15:03:23 +0200

    Add UserService interface and implementation.

---
+++ springBootApp/src/main/java/org/example/springBootApp/service/UserService.java
@@ -0,0 +1,9 @@
+package org.example.springBootApp.service;
+
+import org.example.springBootApp.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> getAllUsers();
+}
---
+++ springBootApp/src/main/java/org/example/springBootApp/service/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package org.example.springBootApp.service;
+
+import org.example.springBootApp.model.User;
+import org.example.springBootApp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
