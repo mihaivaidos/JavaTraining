4d858d05032d17f342890fbf30e5f5b7c9c98a10
mihaivaidos <mihai@vaidos.com>
2025-11-13 14:30:30 +0200

    Remove unnecessary Autowired annotations for constructors in UserController and UserServiceImpl

--- springBootApp/src/main/java/org/example/springBootApp/controller/UserController.java
+++ springBootApp/src/main/java/org/example/springBootApp/controller/UserController.java
@@ -4,7 +4,6 @@ import org.example.springBootApp.model.User;
 import org.example.springBootApp.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +17,6 @@ public class UserController {
     private final UserService userService;
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
-    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
--- springBootApp/src/main/java/org/example/springBootApp/service/UserServiceImpl.java
+++ springBootApp/src/main/java/org/example/springBootApp/service/UserServiceImpl.java
@@ -4,7 +4,6 @@ import org.example.springBootApp.model.User;
 import org.example.springBootApp.repository.UserRepository;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,7 +14,6 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
-    @Autowired
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
