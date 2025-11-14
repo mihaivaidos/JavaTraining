0503aa7b43dbc50c3684cfa950ff1b3dece2398b
mihaivaidos <mihai@vaidos.com>
2025-11-14 10:58:15 +0200

    Update User class with JPA annotations

--- springBootApp/src/main/java/org/example/springBootApp/springDataJPAExercises/model/User.java
+++ springBootApp/src/main/java/org/example/springBootApp/springDataJPAExercises/model/User.java
@@ -1,5 +1,6 @@
 package org.example.springBootApp.springDataJPAExercises.model;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
@@ -8,11 +9,25 @@ import lombok.*;
 @AllArgsConstructor
 @ToString
 @EqualsAndHashCode
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(name = "username")
     private String username;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "firstname")
     private String firstname;
+
+    @Column(name = "lastname")
     private String lastname;
 }
