8baff6dc3358466fc362cce761e695a7a1def2e4
mihaivaidos <mihai@vaidos.com>
2025-11-14 11:37:15 +0200

    Create custom query method in the UserRepository and UserService using Query annotation

--- springBootApp/src/main/java/org/example/springBootApp/springDataJPAExercises/repository/UserRepository.java
+++ springBootApp/src/main/java/org/example/springBootApp/springDataJPAExercises/repository/UserRepository.java
@@ -2,8 +2,12 @@ package org.example.springBootApp.springDataJPAExercises.repository;
 
 import org.example.springBootApp.springDataJPAExercises.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
     User findByUsername(String username);
     User findByEmail(String email);
+
+    @Query("SELECT COUNT(u) FROM User u")
+    long countUsers();
 }
