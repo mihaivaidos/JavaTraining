1cc3c2e6bb79964869483802c4993a50b97071e6
mihaivaidos <mihai@vaidos.com>
2025-11-14 11:13:37 +0200

    Add UserService class that uses the CRUD operations, findByUsername and findByEmail methods from UserRepository

---
+++ springBootApp/src/main/java/org/example/springBootApp/springDataJPAExercises/service/UserService.java
@@ -0,0 +1,46 @@
+package org.example.springBootApp.springDataJPAExercises.service;
+
+import org.example.springBootApp.springDataJPAExercises.model.User;
+import org.example.springBootApp.springDataJPAExercises.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> getUserById(Integer id) {
+        return userRepository.findById(id);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Integer id) {
+        userRepository.deleteById(id);
+    }
+
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+}
