2c847005eac1942cf0dca41e464d60199dee586c
mihaivaidos <mihai@vaidos.com>
2025-11-19 16:04:05 +0200

    Add validation annotations for User class

--- springBootApp/src/main/java/org/example/springBootApp/springDataJPAExercises/model/User.java
+++ springBootApp/src/main/java/org/example/springBootApp/springDataJPAExercises/model/User.java
@@ -1,6 +1,7 @@
 package org.example.springBootApp.springDataJPAExercises.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @Getter
@@ -16,18 +17,28 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     @Column(name = "username")
     private String username;
 
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
     @Column(name = "email")
     private String email;
 
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
     @Column(name = "password")
     private String password;
 
+    @NotBlank(message = "First name is required")
+    @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
     @Column(name = "firstname")
     private String firstname;
 
+    @NotBlank(message = "Last name is required")
+    @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
     @Column(name = "lastname")
     private String lastname;
 }
