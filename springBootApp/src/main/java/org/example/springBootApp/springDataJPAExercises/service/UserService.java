dcc77d7353b379ad2f7de1d58e469bd4f2ecdb7e
mihaivaidos <mihai@vaidos.com>
2025-11-19 15:27:58 +0200

    Update the deleteUser and updateUser methods and add partialUpdateUser method in UserService

--- springBootApp/src/main/java/org/example/springBootApp/springDataJPAExercises/service/UserService.java
+++ springBootApp/src/main/java/org/example/springBootApp/springDataJPAExercises/service/UserService.java
@@ -5,6 +5,7 @@ import org.example.springBootApp.springDataJPAExercises.repository.UserRepositor
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -28,12 +29,51 @@ public class UserService {
         return userRepository.findAll();
     }
 
-    public User updateUser(User user) {
-        return userRepository.save(user);
+    public Optional<User> updateUser(Integer id, User userDetails) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(userDetails.getUsername());
+                    user.setEmail(userDetails.getEmail());
+                    user.setPassword(userDetails.getPassword());
+                    user.setFirstname(userDetails.getFirstname());
+                    user.setLastname(userDetails.getLastname());
+                    return userRepository.save(user);
+                });
     }
 
-    public void deleteUser(Integer id) {
-        userRepository.deleteById(id);
+    public boolean deleteUser(Integer id) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    userRepository.deleteById(id);
+                    return true;
+                })
+                .orElse(false);
+    }
+
+    public Optional<User> partialUpdateUser(Integer id, Map<String, Object> updates) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    updates.forEach((key, value) -> {
+                        switch (key) {
+                            case "username":
+                                user.setUsername((String) value);
+                                break;
+                            case "email":
+                                user.setEmail((String) value);
+                                break;
+                            case "password":
+                                user.setPassword((String) value);
+                                break;
+                            case "firstname":
+                                user.setFirstname((String) value);
+                                break;
+                            case "lastname":
+                                user.setLastname((String) value);
+                                break;
+                        }
+                    });
+                    return userRepository.save(user);
+                });
     }
 
     public User getUserByUsername(String username) {
