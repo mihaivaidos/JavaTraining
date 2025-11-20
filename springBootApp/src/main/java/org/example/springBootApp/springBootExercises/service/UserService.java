261975582d1552046255db229bd2b81b5984d5d5
mihaivaidos <mihai@vaidos.com>
2025-11-20 19:02:24 +0200

    Update UserService to map the User entity to UserDto

--- springBootApp/src/main/java/org/example/springBootApp/springBootExercises/service/UserService.java
+++ springBootApp/src/main/java/org/example/springBootApp/springBootExercises/service/UserService.java
@@ -1,10 +1,10 @@
 package org.example.springBootApp.springBootExercises.service;
 
-import org.example.springBootApp.springBootExercises.model.User;
+import org.example.springBootApp.springBootExercises.dto.UserDto;
 
 import java.util.List;
 
 public interface UserService {
-    List<User> getAllUsers();
-    List<User> getUsersByName(String name);
+    List<UserDto> getAllUsers();
+    List<UserDto> getUsersByName(String name);
 }
--- springBootApp/src/main/java/org/example/springBootApp/springBootExercises/service/UserServiceImpl.java
+++ springBootApp/src/main/java/org/example/springBootApp/springBootExercises/service/UserServiceImpl.java
@@ -1,32 +1,42 @@
 package org.example.springBootApp.springBootExercises.service;
 
-import org.example.springBootApp.springBootExercises.model.User;
+import org.example.springBootApp.springBootExercises.dto.UserDto;
+import org.example.springBootApp.springBootExercises.mapper.UserMapper;
 import org.example.springBootApp.springBootExercises.repository.UserRepository;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
     private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
         this.userRepository = userRepository;
+        this.userMapper = userMapper;
     }
 
     @Override
-    public List<User> getAllUsers() {
+    public List<UserDto> getAllUsers() {
         logger.info("Service: Getting all users.");
-        return userRepository.findAll();
+        return userRepository.findAll()
+                .stream()
+                .map(userMapper::toDto)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public List<User> getUsersByName(String name) {
+    public List<UserDto> getUsersByName(String name) {
         logger.info("Service: Getting users by name filter: {}", name);
-        return userRepository.getUsersByName(name);
+        return userRepository.getUsersByName(name)
+                .stream()
+                .map(userMapper::toDto)
+                .collect(Collectors.toList());
     }
 }
