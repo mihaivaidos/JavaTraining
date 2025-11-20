5f1f3a489511cbe48adc9cd2cb7d2c18b6e78e0b
mihaivaidos <mihai@vaidos.com>
2025-11-20 19:03:12 +0200

    Update UserController methods to use UserDto instead of User

--- springBootApp/src/main/java/org/example/springBootApp/springBootExercises/controller/UserController.java
+++ springBootApp/src/main/java/org/example/springBootApp/springBootExercises/controller/UserController.java
@@ -1,7 +1,7 @@
 package org.example.springBootApp.springBootExercises.controller;
 
 import jakarta.validation.constraints.Size;
-import org.example.springBootApp.springBootExercises.model.User;
+import org.example.springBootApp.springBootExercises.dto.UserDto;
 import org.example.springBootApp.springBootExercises.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,20 +27,20 @@ public class UserController {
     }
 
     @GetMapping
-    public ResponseEntity<List<User>> getAllUsers() {
+    public ResponseEntity<List<UserDto>> getAllUsers() {
         logger.info("Controller: Received GET request for all users.");
-        List<User> users = userService.getAllUsers();
+        List<UserDto> users = userService.getAllUsers();
         logger.info("Controller: Responding with {} total users.", users.size());
         return ResponseEntity.ok(users);
     }
 
     @GetMapping("/search")
-    public ResponseEntity<List<User>> getUsersByName(
+    public ResponseEntity<List<UserDto>> getUsersByName(
             @RequestParam
             @Size(min = 2, max = 50, message = "Name parameter must be between 2 and 50 characters")
             String name) {
         logger.info("Controller: Received GET request to search for users with name: {}", name);
-        List<User> users = userService.getUsersByName(name);
+        List<UserDto> users = userService.getUsersByName(name);
         logger.info("Controller: Responding with {} users found by name.", users.size());
         return ResponseEntity.ok(users);
     }
