a4ad1d551e76b2db9f6765ba929b0c79d1354bb6
mihaivaidos <mihai@vaidos.com>
2025-11-21 15:16:01 +0200

    Add UserDetailsServiceImpl to retrieve user and roles from the database and remove in-memory authentication from SecurityConfig

--- springBootApp/src/main/java/org/example/springBootApp/springSecurityExercises/config/SecurityConfig.java
+++ springBootApp/src/main/java/org/example/springBootApp/springSecurityExercises/config/SecurityConfig.java
@@ -6,9 +6,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -32,22 +29,5 @@ public class SecurityConfig {
             .csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
-    }
-
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails admin = User.builder()
-            .username("admin")
-            .password("{noop}admin123")
-            .roles("ADMIN")
-            .build();
-
-        UserDetails user = User.builder()
-            .username("user")
-            .password("{noop}user123")
-            .roles("USER")
-            .build();
-
-        return new InMemoryUserDetailsManager(admin, user);
     }
 }
